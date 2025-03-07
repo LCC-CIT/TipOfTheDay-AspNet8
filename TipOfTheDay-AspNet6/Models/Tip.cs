63a6f439338d6163f9ca6a297d432cbdd638989f
birdb <birdb@lanecc.edu>
2025-03-06 17:28:19 -0800

    Changes from last year.

--- TipOfTheDay-AspNet6/Models/Tip.cs
+++ TipOfTheDay-AspNet6/Models/Tip.cs
@@ -2,6 +2,7 @@
 {
     public class Tip
     {
+        // Backing variables
         private readonly List<Language> _languages = new();
         private readonly List<Tag> _tags = new();
         private readonly List<Comment> _comments = new();
@@ -9,18 +10,13 @@
 
         public int TipId { get; set; }
 
-        // Reference types are now non-nullable by default
+        // Reference types are now non-nullable by default in .NET 6
         // Member is being made nullable to facilitate the scaffolded Edit feature
         // If non-nullable we get a ModelValidation error if it's not included in a HTTP Post to Edit.
         public AppUser? Member { get; set; }
-
         public List<Language> Languages { get => _languages; }
-
         public List<Tag> Tags{ get => _tags; }
-
         public List<Comment> Comments { get => _comments; }
-
         public String TipText { get => _tipText; set => _tipText = value; }
-
     }
 }
